b647202c83e1c98862ac4c0241009b9f4f8a7087
chunta-chen <chunta.chen@lookout.com>
2022-07-19 18:44:28 +0800

    Add color assets

--- GenPractice/ViewController.swift
+++ GenPractice/ViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let txt = UILabel.init(frame: CGRect(x: 10, y: 110, width: 300, height: 40))
         txt.text = L10n.maliciousContentBody
-        txt.layer.borderWidth = 1
         view.addSubview(txt)
+        view.backgroundColor = Colors.mainBlack.color
+        
+        let btn = UIButton.init(frame: CGRect(x: 10, y: 300, width: 300, height: 40))
+        btn.setTitle("TEST", for: .normal)
+        btn.setTitleColor(Colors.defaultGreen.color, for: .normal)
+        view.addSubview(btn)
     }
-
-
 }
 
